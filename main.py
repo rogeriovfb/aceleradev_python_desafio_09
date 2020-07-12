b4177404ca04f5727a495cea650652d6da320a77
Rogerio Ballestrin <ballestrin.rogerio@gmail.com>
2020-07-12 10:10:49 -0300

    Score: 100%

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 # .travis.yml
 config = """
-
+language: python
+python:
+  - "2.7"
+  - "pypy"
+  - "3.7"
+  - "pypy3"
+  
+stages: 
+  - name: test
+  
+install:
+  - pip install -r requirements.txt
+  
+script: pytest
 """
